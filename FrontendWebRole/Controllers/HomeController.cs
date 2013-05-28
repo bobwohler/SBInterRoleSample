46dcf91f6f3002192d0a0b9ea9e6a9544bdb7543
Bob Wohler <bobwohler@gamestop.com>
2013-05-28 11:00:46 -0500

    Initial commit
    Partial implementation of sample.

---
+++ FrontendWebRole/Controllers/HomeController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FrontendWebRole.Models;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+
+namespace FrontendWebRole.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            // Simply redirect to Submit, since Submit
+            // will serve as the front page of this application.
+            return RedirectToAction("Submit");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+        #region Submit Actions
+
+        /// <summary>
+        /// GET: /Home/Submit
+        /// Controller method for for the submission form view.
+        /// </summary>
+        /// <returns>Submission Form View</returns>
+        public ActionResult Submit()
+        {
+            // As usual, need to get a namespace manager for 
+            // management operations.
+            var namespaceManager = QueueConnector.CreateNamespaceManager();            
+
+            // Get the queue and obtain the message count.
+            var queue = namespaceManager.GetQueue(QueueConnector.QueueName);
+            ViewBag.MessageCount = queue.MessageCount;
+            return View();
+        }
+
+        /// <summary>
+        /// POST: /Home/Submit
+        /// Controller method for handling submissions from the submissions form.
+        /// </summary>
+        /// <returns>RedirectToAction</returns>
+        [HttpPost]
+        // Attribute to help prevent cross-site scripting attacks and cross-site request forgery.
+        [ValidateAntiForgeryToken]
+        public ActionResult Submit(OnlineOrder order)
+        {
+            // Validate model state and if good
+            // submit to the SB queue.
+            if (ModelState.IsValid)
+            {
+                // Create a brokered message for the incoming Order 
+                // and submit it to the queue.
+                // By default this constructor will use the
+                // DataContract Serializer.
+                var message = new BrokeredMessage(order);
+                
+                // Send it to the queue.
+                QueueConnector.OrdersQueueClient.Send(message);
+                
+                return RedirectToAction("Submit");
+            }
+            else
+            {
+                return View(order);
+            }
+        }
+        
+        #endregion
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ FrontendWebRole/QueueConnector.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+
+namespace FrontendWebRole
+{
+    public class QueueConnector
+    {
+        /// <summary>
+        /// Thread safe. Recommended to cache 
+        /// rather than recreating it on every 
+        /// request.
+        /// </summary>
+        public static QueueClient OrdersQueueClient;
+
+
+        /// <summary>
+        /// These values are defined in the Azure
+        /// Management Portal.
+        /// </summary>
+        public const string Namespace = "SBInterRoleSample";
+        public const string IssuerName = "owner";
+        public const string IssuerKey = "2LjZKry7d2+MAKQ7ipdKXB7Rr/CgjaGMyHQyZ9S83Ao=";
+
+        public const string QueueName = "OrdersQueue";
+
+        /// <summary>
+        /// Create the namespace manager which gives
+        /// you access to management operations.
+        /// </summary>
+        /// <returns></returns>
+        public static NamespaceManager CreateNamespaceManager()
+        {
+            var uri = ServiceBusEnvironment.CreateServiceUri("sb", Namespace, string.Empty);
+            var tP = TokenProvider.CreateSharedSecretTokenProvider(IssuerName, IssuerKey);
+            return new NamespaceManager(uri, tP);
+        }
+
+        public static void Initialize()
+        {
+            // Use Http to be friendly with outbound firewalls.
+            ServiceBusEnvironment.SystemConnectivity.Mode =
+                ConnectivityMode.Http;
+
+            // Get an instance of the namespace manager
+            // to get access to management operations.
+            var namespaceManager = CreateNamespaceManager();
+
+            // Create the queue if it does not exist already.
+            if (!namespaceManager.QueueExists(QueueName))
+            {
+                namespaceManager.CreateQueue(QueueName);
+            }
+
+            // Get a client to the queue.
+            var messagingFactory = MessagingFactory.Create(
+                namespaceManager.Address,
+                namespaceManager.Settings.TokenProvider);
+            OrdersQueueClient = messagingFactory.CreateQueueClient(QueueName);
+        }
+    }
+}
